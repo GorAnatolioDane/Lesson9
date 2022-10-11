38cac3dfe82f5ab734a7c2e440e8c7d0c5bf67db
Dane <slip_dane@mail.ru>
2022-10-11 20:56:48 +0400

    AddTask1

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void printInConsoleWithColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(information);
+    Console.ResetColor();
+}
+int Exponentiation(int firstNumber, int secondNumber)
+{
+    int result = 1;
+    for(int i=1; i <= secondNumber; i++) 
+    {
+        result = result * firstNumber;
+    }
+    return result;
+}
+printInConsoleWithColor($"Введите число,которое хотите возвести в степень:",ConsoleColor.DarkBlue);
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+printInConsoleWithColor($"Введите степень:",ConsoleColor.DarkBlue);
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int exponentiation = Exponentiation(firstNumber, secondNumber);
+Console.WriteLine($"Число {firstNumber} в степени {secondNumber} равняется {exponentiation}");
