d43d5249280b78c5e797c46b0d6f1f1696129d04
Dane <slip_dane@mail.ru>
2022-10-12 00:12:35 +0400

    AddTask3

---
+++ Lesson4/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿void printInConsoleWithColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(information);
+    Console.ResetColor();
+}
+
+printInConsoleWithColor("Введите длинну массива: ",ConsoleColor.DarkBlue);
+int userArrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[userArrayLength];
+for (int i = 0; i < array.Length; i++) 
+{
+    array[i] = new Random().Next(1, 99);
+}
+printArray(array);
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+      Console.Write(array[i]);  
+      if(i < array.Length - 1)
+      {
+        Console.Write(",");  
+      }
+    }
+    Console.Write("]");
+}
+
+
