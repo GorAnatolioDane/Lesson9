9241e6801c6a63371fdf6af1f886c65c5eb8abff
Dane <slip_dane@mail.ru>
2022-10-13 00:27:13 +0400

    AddTask34

---
+++ Lesson5/Task34/Program.cs
@@ -0,0 +1,52 @@
+﻿void printInConsoleWithColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(information);
+    Console.ResetColor();
+}
+
+printInConsoleWithColor("Введите длинну массива: ",ConsoleColor.DarkBlue);
+int userArrayLength = Convert.ToInt32(Console.ReadLine());
+
+int[] getRandomArray(int userArrayLength, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[userArrayLength];
+    for (int i = 0; i < userArrayLength; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for(int i = 0; i < incomingArray.Length; i++)
+    {
+      Console.Write(incomingArray[i]);  
+      if(i < incomingArray.Length - 1)
+      {
+        Console.Write(",")  ;  
+      }
+    }
+    Console.WriteLine("]");
+}
+int getEvenNumbersInArray(int[] incomingArray)
+{
+    int count = 0;
+    for(int i = 0; i < incomingArray.Length; i++)
+    {  
+      if(incomingArray[i] % 2 == 0)
+      {
+        count += 1;     
+      }
+    }
+    return count;
+}
+
+int [] currentArray = getRandomArray(userArrayLength, 99, 999);
+printArray(currentArray);
+
+int EvenNumbersInArray = getEvenNumbersInArray(currentArray);
+
+printInConsoleWithColor($"Колличество четных чисел в массиве равно : {EvenNumbersInArray}",ConsoleColor.DarkMagenta);
