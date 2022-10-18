6975efdcad4598dc0f1be8c32fa65cb61959c05d
Dane <slip_dane@mail.ru>
2022-10-18 23:50:28 +0400

    AddTask66

---
+++ Lesson9/Task66/Program.cs
@@ -0,0 +1,36 @@
+﻿void printInConsoleWithColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(information);
+    Console.ResetColor();
+}
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    printInConsoleWithColor($"Сумма элементов = {summ} ",ConsoleColor.DarkBlue);
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
